e5d4a3d47fcd66d1f4486d91955e6267462d16ff
Jose Antonio Rubio <jose.rubio@gambyt.com>
2023-05-19 19:16:19 -0400

    slight structure rearrangings and new flavicon

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMDb Clobe</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta name='description' content='IMDb clone built with nextJS' />
+      <link rel='icon' href='/favicon.ico' />
+    </>
+  );
+}
